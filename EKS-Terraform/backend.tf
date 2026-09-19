75365e40686e16810fe19f0b506d51f59f787ea8
praveenjoseph425-afk <praveenjoseph425@gmail.com>
2026-09-19 16:50:15 +0530

    Change S3 bucket name for Terraform backend

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevopsss"
+    bucket       = "gundethochusa"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
