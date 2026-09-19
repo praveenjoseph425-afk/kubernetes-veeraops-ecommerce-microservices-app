3c86ec2f2608971958d2220516cfc2f5f6942a75
praveenjoseph425-afk <praveenjoseph425@gmail.com>
2026-09-19 20:18:38 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "gundethochusa"
+    bucket       = "gudumbashankar"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
